221e01c521504e553f6b1f921daaebef55642901
Puika <max_fabian@hotmail.com.ar>
2014-10-09 19:31:11 -0300

    Pequeño cambio boludo.

--- HTTPRequests.cs
+++ HTTPRequests.cs
@@ -77,33 +77,8 @@ namespace ClassLibrary1
 
         public string postRequest(string Url,string postData){
 
-            StreamReader ddResponse;
-            ASCIIEncoding ascii = new ASCIIEncoding();
-            byte[] postBytes = ascii.GetBytes(postData.ToString());
+            return postRequest(Url, postData, false);
 
-            request = (HttpWebRequest)HttpWebRequest.Create(Url);
-            request.CookieContainer = cookieCont;
-  
-            request.Method = "POST";
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.ContentLength = postBytes.Length;
-            //request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36";
-            request.UserAgent = "PTaringa/2.2";
-            request.Headers["X-Requested-With"]="XMLHttpRequest";
-            request.Headers["Origin"] = "http://www.taringa.net";
-            //request.Host = "http://www.taringa.net";
-            request.Referer = "http://www.taringa.net/";
-            request.Accept = "application/json, text/javascript, */*; q=0.01";
-
-            Stream postStream = request.GetRequestStream();
-            postStream.Write(postBytes, 0, postBytes.Length);
-
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            ddResponse = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
-            postStream.Flush();
-            postStream.Close();
-
-            return ddResponse.ReadToEnd();
         }
         public string postRequest(string Url, string postData,bool isAjaxRequest)
         {
